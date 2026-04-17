bab6907f6c319dc8eb280cb9f091a4200ae56c49
Marcos de Carvalho <mocarv@proton.me>
2026-04-17 09:05:29 -0300

    Add initial diagram for GULECD-UFPR website structure

---
+++ estruturacao/gulecd-website.mm
@@ -0,0 +1,251 @@
+
+<map version="0.9.0">
+    <node TEXT="Diagrama GULECD-UFPR Website" FOLDED="false" POSITION="right" ID="69e10308e4f880af7dfdcb3c" X_COGGLE_POSX="-624" X_COGGLE_POSY="-72">
+        <edge COLOR="#d0d0d0"/>
+        <font NAME="Helvetica" SIZE="17"/>
+        <node TEXT="Home" FOLDED="false" POSITION="right" ID="5c129ca86b868944a5a808d1">
+            <edge COLOR="#67d7c4"/>
+            <font NAME="Helvetica" SIZE="15"/>
+            <node TEXT="S - Sobre o GULECD - UFPR" FOLDED="false" POSITION="right" ID="dba9c73b1046da35629a9b90">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Nossa missão" FOLDED="false" POSITION="right" ID="302941c807198dcf2ccce508">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Objetivos" FOLDED="false" POSITION="right" ID="c1631df8bd11636d35f2a5be">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Novidades" FOLDED="false" POSITION="right" ID="b6a8722acbeee4fbe6bc2e87">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="P - Notícia 1" FOLDED="false" POSITION="right" ID="32f0f2d0c92a6d3611abe5e8">
+                    <edge COLOR="#67d7c4"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="P - Notícia 2" FOLDED="false" POSITION="right" ID="3daa344563e07ccddc03097a">
+                    <edge COLOR="#67d7c4"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="P - Notícia X" FOLDED="false" POSITION="right" ID="51a3118a79c9327fdeea909f">
+                    <edge COLOR="#67d7c4"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="S - Parceiros" FOLDED="false" POSITION="right" ID="656270382fab7729e0332093">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Faça parte" FOLDED="false" POSITION="right" ID="ee03f146720284f5a54f6461">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Apoie" FOLDED="false" POSITION="right" ID="4e320dee600045a8f4c6d8f9">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Contato (form)" FOLDED="false" POSITION="right" ID="5d02731cc975ffce325e8d01">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="S - Social" FOLDED="false" POSITION="right" ID="eaa9e2b7f8420d068c5bcfb4">
+                <edge COLOR="#ebd95f"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Mastodon" FOLDED="false" POSITION="right" ID="523bab6b195563470097b61d">
+                    <edge COLOR="#e9d459"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - BlueSky" FOLDED="false" POSITION="right" ID="70b1c23b1be0c128d3afe239">
+                    <edge COLOR="#ebd765"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Github" FOLDED="false" POSITION="right" ID="a7c9f97e278c7f061ad60d27">
+                    <edge COLOR="#ebd867"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - LIbreTube" FOLDED="false" POSITION="right" ID="035b70322a213304a17776ba">
+                    <edge COLOR="#ecd85e"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Pixelfed" FOLDED="false" POSITION="right" ID="2bf51eb927f83c1ff18e8796">
+                    <edge COLOR="#ecd864"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="P -Manifesto" FOLDED="false" POSITION="right" ID="234a6577909cfc6fc9ad8130">
+                <edge COLOR="#61d7c1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+            </node>
+            <node TEXT="P - Membros" FOLDED="false" POSITION="right" ID="7ba6c44590b03a0f8e6ab1d6">
+                <edge COLOR="#6fd7c4"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Membros Fundadores" FOLDED="false" POSITION="right" ID="4afd62159c98f2cf0c5c8a1b">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Lista de Membros Fundadores" FOLDED="false" POSITION="right" ID="facc4b31fff735db1ba8b120">
+                        <edge COLOR="#6ad2bc"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Membros Cadastrados" FOLDED="false" POSITION="right" ID="a44cb7585613065654bd5462">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Lista de Membros Ativos Cadastrados" FOLDED="false" POSITION="right" ID="09802726198a08b47c0cc042">
+                        <edge COLOR="#74dac3"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+            </node>
+            <node TEXT="P -Coordenação" FOLDED="false" POSITION="right" ID="cc7adfaf410eadc309985818">
+                <edge COLOR="#5dd7c0"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Coordenação em Exercício" FOLDED="false" POSITION="right" ID="b124577ed5c37f7f7b1a0788">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Coordenações anteriores" FOLDED="false" POSITION="right" ID="fdac9abf996da3f42443b93b">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Gestão XX" FOLDED="false" POSITION="right" ID="c803164345472344ca631181">
+                        <edge COLOR="#60d8be"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                    <node TEXT="P - Gestão XY" FOLDED="false" POSITION="right" ID="4a1b386eec4f1230081e0369">
+                        <edge COLOR="#69d9c0"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+            </node>
+            <node TEXT="P - Regulamentos" FOLDED="false" POSITION="right" ID="be3686104b8382ce89232ff5">
+                <edge COLOR="#62d6c0"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Carta de Fundação" FOLDED="false" POSITION="right" ID="68306751d3a17b73925d604d">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Texto da Carta de Fundação" FOLDED="false" POSITION="right" ID="bd35f93a0f1f839b0bce2f48">
+                        <edge COLOR="#53d7ba"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Regimento Interno" FOLDED="false" POSITION="right" ID="639354c2418b7ef0231c9531">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Texto do Regimento Interno" FOLDED="false" POSITION="right" ID="997483e49035416880176b0d">
+                        <edge COLOR="#73d9c2"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Regulamento Eleitoral" FOLDED="false" POSITION="right" ID="489e26eead758f5955ff146e">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Texto do Regulamento Eleitoral" FOLDED="false" POSITION="right" ID="2c1ad6a54070550c0adb7c21">
+                        <edge COLOR="#60dabf"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Código de Conduta" FOLDED="false" POSITION="right" ID="e229b2a2df2c58040714d0cc">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Texto do Código de Conduta" FOLDED="false" POSITION="right" ID="da2966f6cd379f7170ff4472">
+                        <edge COLOR="#52d4b7"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+            </node>
+            <node TEXT="P -Projetos" FOLDED="false" POSITION="right" ID="d87dcf3aa29ddc42bdaa264d">
+                <edge COLOR="#60d8c1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Biblioteca de Distros Linux" FOLDED="false" POSITION="right" ID="fdb3d235642963bd9283d553">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - BLIUX - Biblioteca de Distros Linux" FOLDED="false" POSITION="right" ID="8b88817cd1f5ad7b6a07dbc7">
+                        <edge COLOR="#67d7c4"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                        <node TEXT="S - Sobre" FOLDED="false" POSITION="right" ID="def3b719db92380dfb3a1d0a">
+                            <edge COLOR="#ebd95f"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                        <node TEXT="S - Objetivos" FOLDED="false" POSITION="right" ID="482fdb6c12951e33c096cb4c">
+                            <edge COLOR="#ebd95f"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                        <node TEXT="S - Regulamento" FOLDED="false" POSITION="right" ID="6d38d611dd0eb10e914d7a10">
+                            <edge COLOR="#ebd95f"/>
+                            <font NAME="Helvetica" SIZE="12"/>
+                        </node>
+                    </node>
+                </node>
+                <node TEXT="S - Atividades Educacionais" FOLDED="false" POSITION="right" ID="f4e91f8d6ed2d041589e8b0d">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Suporte a usuários Linux" FOLDED="false" POSITION="right" ID="9ea22ad3493319f5f00dab4a">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Install Party" FOLDED="false" POSITION="right" ID="ada72f7438dd5e57b51f4114">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Encontros" FOLDED="false" POSITION="right" ID="76d12cebe706c09609476ff3">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Eventos" FOLDED="false" POSITION="right" ID="24ff661a8afa6d2276a96441">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="P - Recursos" FOLDED="false" POSITION="right" ID="4c04baf50c16dff67478e960">
+                <edge COLOR="#5fd7c1"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Tutoriais" FOLDED="false" POSITION="right" ID="856d158e82a10ea9910a7f4a">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Tutorial X" FOLDED="false" POSITION="right" ID="768ce4edbfa298bbde926e3b">
+                        <edge COLOR="#64d8bf"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                    <node TEXT="P - Tutorial Y" FOLDED="false" POSITION="right" ID="00e32c8705505448b73d9d9a">
+                        <edge COLOR="#62d6bd"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Artigos" FOLDED="false" POSITION="right" ID="f5a938b7e9ebf998a3656e92">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                    <node TEXT="P - Artigo X" FOLDED="false" POSITION="right" ID="46a1d55e281082de9b1ffadd">
+                        <edge COLOR="#56d6bb"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                    <node TEXT="Artigo Y" FOLDED="false" POSITION="right" ID="4cdd17cc14b59ae1d257fc00">
+                        <edge COLOR="#5ed6bc"/>
+                        <font NAME="Helvetica" SIZE="12"/>
+                    </node>
+                </node>
+                <node TEXT="S - Links" FOLDED="false" POSITION="right" ID="e81129950f52b65b03e2eabd">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+            <node TEXT="P - Materiais Gráficos" FOLDED="false" POSITION="right" ID="33145cad7c571ef4e4d83b48">
+                <edge COLOR="#5ed4be"/>
+                <font NAME="Helvetica" SIZE="13"/>
+                <node TEXT="S - Logos" FOLDED="false" POSITION="right" ID="c4be656df3173bc783e82b15">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+                <node TEXT="S - Wallpapers" FOLDED="false" POSITION="right" ID="ae8a33aedc61bae62c5832ba">
+                    <edge COLOR="#ebd95f"/>
+                    <font NAME="Helvetica" SIZE="12"/>
+                </node>
+            </node>
+        </node>
+    </node>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="d7c13b302aa77f5ac6812a6c" X_COGGLE_POSX="-447" X_COGGLE_POSY="105">
+        <edge COLOR="#9ed56b"/>
+        <font NAME="Helvetica" SIZE="15"/>
+    </x-coggle-rootnode>
+</map>
